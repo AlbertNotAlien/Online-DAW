5d5616c8c360e467cbf466ffdc34eba3a6e806b9
AlbertNotAlien <albert830207@gmail.com>
2022-10-31 15:48:01 +0800

    fix: display two times by strict mode

--- pages/components/Track/WaveSurferNext.jsx
+++ pages/components/Track/WaveSurferNext.jsx
@@ -1,29 +1,71 @@
 // npm install wavesurfer.js
-// import Timeline from "wavesurfer.js/dist/plugin/wavesurfer.timeline.js";
-
 import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 
-const Bar = styled.div`
-  width: 500px;
+import data from "./data-structure";
+
+console.log(data);
+
+const bpm = data.projects[0].tempo;
+const duration = 120.0; // 120.000
+const barWidth = 10; // 一個bar長10px
+const barQuantity = duration * (bpm / 60); // 產生的小節數量
+const totalWidth = barWidth * barQuantity; // 總長度
+
+const Track = styled.div`
+  /* width: 100%; */
+  width: ${totalWidth}px;
+  position: relative;
 `;
 
-const formWaveSurferOptions = (waveformRef, timelineRef) => ({
+const TimelineBlock = styled.div`
+  width: ${totalWidth}px;
+  /* width: 500px; */
+  height: 100px;
+`;
+
+const Bars = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  z-index: 0;
+`;
+
+const FlexBars = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Bar = styled.div`
+  width: 1px;
+  height: 150px;
+  border-left: 0.1px solid gray;
+  padding: 0 ${barWidth}px;
+  cursor: pointer;
+  &:hover {
+    border-left: 0.1px solid white;
+  }
+`;
+
+const BarLight = styled(Bar)`
+  background-color: #2d2d2d;
+`;
+
+const BarDark = styled(Bar)`
+  background-color: #141414;
+`;
+
+const formWaveSurferOptions = (waveformRef) => ({
   container: waveformRef,
   waveColor: "#eee",
   progressColor: "#0178FF",
   cursorColor: "OrangeRed",
-  // barWidth: 3,
-  // barRadius: 3,
   responsive: true,
   height: 150,
   normalize: true,
   partialRender: true,
-  plugins: [
-    // Timeline.create({
-    //   container: timelineRef,
-    // }),
-  ],
+  fillParent: false,
+  plugins: [],
 });
 
 function WaveSurferNext() {
@@ -33,69 +75,24 @@ function WaveSurferNext() {
   const [playing, setPlaying] = useState(false);
   const [progress, setProgress] = useState(0);
 
-  console.log(Math.floor(progress));
-
   const url =
     "https://www.mfiles.co.uk/mp3-downloads/brahms-st-anthony-chorale-theme-two-pianos.mp3";
 
+  const create = async () => {
+    const WaveSurfer = (await import("wavesurfer.js")).default;
+
+    const options = formWaveSurferOptions(waveformRef.current);
+    wavesurfer.current = WaveSurfer.create(options);
+
+    wavesurfer.current.load(url);
+  };
+
   useEffect(() => {
-    const create = async () => {
-      const WaveSurfer = (await import("wavesurfer.js")).default;
-      const MarkersPlugin = (await import("wavesurfer.js/src/plugin/markers"))
-        .default;
-      const TimelinePlugin = (await import("wavesurfer.js/src/plugin/timeline"))
-        .default;
-
-      const options = formWaveSurferOptions(
-        waveformRef.current,
-        timelineRef.current
-      );
-      options.plugins.push(
-        MarkersPlugin.create({
-          markers: [
-            {
-              time: 58,
-              label: "",
-              color: "#ff990a",
-            },
-            {
-              time: 5.5,
-              label: "",
-              color: "#ff990a",
-            },
-
-            {
-              time: 24,
-              label: "END",
-              color: "#00ffcc",
-              position: "top",
-            },
-          ],
-        }),
-        TimelinePlugin.create({
-          container: timelineRef,
-        })
-      );
-
-      wavesurfer.current = WaveSurfer.create(options);
-      wavesurfer.current.load(url);
-      // wavesurfer.current.load(require(url));
-
-      wavesurfer.current.on("marker-click", function (marker) {
-        console.log("marker drop", marker);
-      });
-
-      wavesurfer.current.on("audioprocess", function () {
-        const currentTime = wavesurfer.current.getCurrentTime();
-        setProgress(currentTime);
-      });
-    };
-
     create();
 
     return () => {
+      console.log(wavesurfer.current);
       if (wavesurfer.current) {
-        console.log("destroy");
         wavesurfer.current.destroy();
       }
     };
@@ -106,22 +103,43 @@ function WaveSurferNext() {
     wavesurfer.current.playPause();
   };
 
-  const back30 = () => {
-    wavesurfer.current.skipBackward(30);
-  };
+  // const back30 = () => {
+  //   wavesurfer.current.skipBackward(30);
+  // };
 
-  const forward30 = () => {
-    wavesurfer.current.skipForward(30);
-  };
+  // const forward30 = () => {
+  //   wavesurfer.current.skipForward(30);
+  // };
 
   return (
     <div>
-      <Bar id="waveform" ref={waveformRef} />
-      <Bar ref={timelineRef} />
+      <Track id="waveform" ref={waveformRef}>
+        <Bars>
+          {new Array(parseInt(barQuantity / 8)).fill(0).map((_, index) => {
+            return (
+              <FlexBars key={index}>
+                {new Array(4).fill(0).map((_, index) => {
+                  return (
+                    <div key={index}>
+                      <BarLight />
+                    </div>
+                  );
+                })}
+                {new Array(4).fill(0).map((_, index) => {
+                  return (
+                    <div key={index}>
+                      <BarDark />
+                    </div>
+                  );
+                })}
+              </FlexBars>
+            );
+          })}
+        </Bars>
+      </Track>
+      <TimelineBlock id="wave-timeline" ref={timelineRef} />
       <div className="controls">
-        <div onClick={back30}>Back 30</div>
         <button onClick={handlePlayPause}>{!playing ? "Play" : "Pause"}</button>
-        <div onClick={forward30}>Forward 30</div>
       </div>
       <div className="progress">{Math.floor(progress)}</div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        {/* <Waveform /> */}
-        <WaveSurferNext />
-      </main>
-
-      {/* <footer className={styles.footer}></footer> */}
+      <WaveSurferNext />
     </div>
   );
 }
