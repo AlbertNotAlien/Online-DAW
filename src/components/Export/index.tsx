e0bd2a432730e13395a79c54b184faeec49d552e
AlbertNotAlien <albert830207@gmail.com>
2022-11-15 20:18:00 +0800

    feat: setting export from useRecorder

--- src/components/Export/index.tsx
+++ src/components/Export/index.tsx
@@ -15,23 +15,63 @@ const Export = () => {
     // const OutgoingAudioMediaStream = new MediaStream();
     // OutgoingAudioMediaStream.addTrack(OutgoingStream.getAudioTracks()[0]);
 
+    // if (tracksData) {
+    //   console.log(tracksData[0].clips[0].url);
+
+    //   const url = tracksData[0].clips[0].url;
+    //   const audio = new Audio(url);
+    //   const ctx = new window.AudioContext();
+    //   const source = ctx.createMediaElementSource(audio);
+    //   const streamDest = ctx.createMediaStreamDestination();
+    //   const audioNode = source.connect(streamDest);
+
+    //   const dest = ctx.createMediaStreamDestination();
+    //   dest.connect(audioNode);
+
+    //   const finalStream = dest.stream;
+    // }
     if (tracksData) {
-      console.log(tracksData[0].clips[0].url);
+      // console.log(tracksData[0].clips[0].url);
 
       const url = tracksData[0].clips[0].url;
       const audio = new Audio(url);
+      audio.crossOrigin = "anonymous";
       const ctx = new window.AudioContext();
-      const streamDest = ctx.createMediaStreamDestination();
       const source = ctx.createMediaElementSource(audio);
-      const audioNode = source.connect(streamDest);
-      console.log("source", source);
-
+      // console.log("source", source);
+      // const streamDest = ctx.createMediaStreamDestination();
+      // console.log("streamDest", streamDest);
       const dest = ctx.createMediaStreamDestination();
-      console.log("dest", dest);
-      dest.connect(audioNode);
+      const finalDest = source.connect(dest);
+      // console.log("finalDest", finalDest);
+
+      // console.log("dest", dest);
+      // audioNode.connect(dest);
 
       const finalStream = dest.stream;
-      console.log(finalStream);
+      // console.log(finalStream);
+
+      const recorder = new MediaRecorder(finalStream);
+      console.log("recorder", recorder);
+
+      let audioChunks: any[] = [];
+      const handleData = (e: any) => {
+        console.log("handleData");
+        audioChunks.push(e.data);
+        console.log("e.data", e.data);
+        let blob = new Blob(audioChunks, { type: "audio/mp3" });
+        console.log("blob", blob);
+      };
+
+      recorder.start();
+      recorder.addEventListener("dataavailable", handleData);
+      // recorder.addEventListener("dataavailable", function (e) {
+      //   console.log("file");
+      //   const file = new File([e.data], "fileName", {
+      //     type: "audio/mp3",
+      //   });
+      //   console.log(file);
+      // });
     }
   };
   return <button onClick={exportAudio}>export</button>;
